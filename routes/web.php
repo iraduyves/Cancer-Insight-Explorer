6b76bbcb91e895e13ae2ea10964bc5579acd8994
Yves IRADUKUNDA <iradukundayves11@gmail.com>
2024-01-15 22:55:07 +0200

    Add more functionality

--- routes/web.php
+++ routes/web.php
@@ -35,4 +35,4 @@ Route::get('/showappointment',[AdminController::class,'showappointment']);
 Route::post('/appointment',[HomeController::class,'appointment']);
 Route::get('/myappointment',[HomeController::class,'myappointment']);
 Route::get('/cancel_appoint/{id}',[HomeController::class,'cancel_appoint']);
-kkjhgghjkljh
+kkjhgghjklj
