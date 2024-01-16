02f8bdf6348fa8a65239da90ac81add13a66f55f
Yves IRADUKUNDA <iradukundayves11@gmail.com>
2024-01-16 22:20:24 +0200

    feat:finilasing the project fro Demo

--- resources/views/admin/Sidebar.blade.php
+++ resources/views/admin/Sidebar.blade.php
@@ -56,7 +56,6 @@
       <li class="nav-item nav-category">
         <span class="nav-link">Navigation</span>
       </li>
-uhgsbbcv
       <li class="nav-item menu-items">
         <a class="nav-link" href="{{url('add_doctor_view')}}">
           <span class="menu-icon">
