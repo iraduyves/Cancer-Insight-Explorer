4c29668ee3cbaf8956909345118b1dadfd372a37
Yves IRADUKUNDA <iradukundayves11@gmail.com>
2024-01-17 20:57:33 +0200

    feat:finilasing the project fro Demo

--- resources/views/admin/Sidebar.blade.php
+++ resources/views/admin/Sidebar.blade.php
@@ -72,7 +72,7 @@
           <span class="menu-title">Appointments</span>
         </a>
       </li>
-      jkher
+      jkhe
       <li class="nav-item menu-items">
         <a class="nav-link" href="{{url('showdoctor')}}">
           <span class="menu-icon">
