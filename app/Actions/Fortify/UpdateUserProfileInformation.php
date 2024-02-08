2aea2003ed624516a2e154be60cc98b31018e12c
Yves IRADUKUNDA <iradukundayves11@gmail.com>
2024-02-08 22:03:46 +0200

    fix:fixing some bugs

--- app/Actions/Fortify/UpdateUserProfileInformation.php
+++ app/Actions/Fortify/UpdateUserProfileInformation.php
@@ -8,6 +8,7 @@ use Illuminate\Support\Facades\Validator;
 use Illuminate\Validation\Rule;
 use Laravel\Fortify\Contracts\UpdatesUserProfileInformation;
 
+
 class UpdateUserProfileInformation implements UpdatesUserProfileInformation
 {
     /**
