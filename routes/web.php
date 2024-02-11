280f0aaa1ad89b2564dc8d121d05030e91e1c812
Yves IRADUKUNDA <iradukundayves11@gmail.com>
2024-02-11 23:37:43 +0200

    fix:fixing routing bugs

--- routes/web.php
+++ routes/web.php
@@ -71,4 +71,3 @@ Route::get('/cancel_appoint/{id}',[HomeController::class,'cancel_appoint']);
 Route::get('/cancel_appoint/{id}',[HomeController::class,'cancel_appoint']);
 Route::get('/cancel_appoint/{id}',[HomeController::class,'cancel_appoint']);
 Route::get('/cancel_appoint/{id}',[HomeController::class,'cancel_appoint']);
-Route::get('/cancel_appoint/{id}',[HomeController::class,'cancel_appoint']);
